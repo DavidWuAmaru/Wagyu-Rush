2a7c9d0951bae77cddd0c08ed935414725a0e5d8
Hao-Wen Chiang <jeff99998888@yahoo.com>
2021-11-03 15:27:23 +0800

    沒事

--- Assets/Hao-Wen/Scripts/CheckGridSize.cs
+++ Assets/Hao-Wen/Scripts/CheckGridSize.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class CheckGridSize : MonoBehaviour
 {
-    private float gridWidth;
-    private float gridHeight;
-    private Bounds gridBound;
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +15,6 @@ public class CheckGridSize : MonoBehaviour
     
     void Update()
     {
-        this.transform.position = GameObject.FindGameObjectWithTag("FuckingPosition").transform.position;
-        Debug.Log(transform.position);
+        
     }
 }

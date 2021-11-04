d625fbb391290c65f1a403199f31464f11092124
Xiang <bosszheng220@gmail.com>
2021-11-04 21:37:09 +0800

    Map editor UI update-to-date

---
+++ Assets/Tom/MapEditor/Scripts/Page.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : MonoBehaviour
+{
+    static int currentPage = 0;
+    [SerializeField] private List<GameObject> pages;
+    [SerializeField] private bool goup = false;
+    private void OnMouseDown()
+    {
+        if (goup && currentPage + 1 < pages.Count)
+        {
+            pages[currentPage].SetActive(false);
+            currentPage++;
+            pages[currentPage].SetActive(true);
+        }
+        else if(!goup && currentPage > 0)
+        {
+            pages[currentPage].SetActive(false);
+            currentPage--;
+            pages[currentPage].SetActive(true);
+        }
+    }
+}

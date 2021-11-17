be59ba3926b877893ef00fc179660bd05dc78fb3
david <daviddavid123.133@gmail.com>
2021-11-18 00:23:02 +0800

    牛牛動畫 在 Scenes dance

--- Assets/David/Scripts/dance.cs
+++ Assets/David/Scripts/dance.cs
@@ -6,56 +6,80 @@ public class dance : MonoBehaviour
 {
     List<Sprite> sprites;
     public Animator playerAnimator;
-    private int direction;
+    private Vector3 target;
+    public float step;
+    private int[] direction;
     void Start()
     {
-        sprites = new List<Sprite>();
-        playerAnimator.enabled = false;
-        sprites.Add(Resources.Load<Sprite>("cow_up"));
-        sprites.Add(Resources.Load<Sprite>("cow_down"));
-        sprites.Add(Resources.Load<Sprite>("cow_left"));
-        sprites.Add(Resources.Load<Sprite>("cow_right"));
+        playerAnimator = gameObject.GetComponent<Animator>();
+        target = this.gameObject.transform.position;
+        step = 1f;
+        direction = new int[] { 0, 0 };
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
-        if (playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
-        {
-            //print("haha");
-            playerAnimator.enabled = false;
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            playerAnimator.enabled = true;
-            playerAnimator.Play("portal_in");
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            playerAnimator.enabled = true;
-            playerAnimator.Play("portal_out");
-        }
-
         if (Input.GetKeyDown(KeyCode.W))
         {
-            direction = 0;
+            target = this.gameObject.transform.position + new Vector3(0, step, 0);
+            direction[0] = 1;
+            direction[1] = 0;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            direction = 1;
+            target = this.gameObject.transform.position + new Vector3(0, -step, 0);
+            direction[0] = -1;
+            direction[1] = 0;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            direction = 2;
+            target = this.gameObject.transform.position + new Vector3(-step, 0, 0);
+            direction[0] = 0;
+            direction[1] = -1;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            direction = 3;
+            target = this.gameObject.transform.position + new Vector3(step, 0, 0);
+            direction[0] = 0;
+            direction[1] = 1;
         }
-        if (gameObject.GetComponent<SpriteRenderer>().sprite != sprites[direction])
+
+        if (this.gameObject.transform.position != target)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = sprites[direction];
+            ani(direction);
+            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, target, 0.01f);
+            playerAnimator.SetBool("arrive_target", false);
         }
+        else
+        {
+            playerAnimator.SetBool("arrive_target", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            playerAnimator.SetBool("sleep", true);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            playerAnimator.SetBool("dance", true);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            playerAnimator.SetBool("dance", false);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            playerAnimator.SetInteger("portal", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            playerAnimator.SetInteger("portal", 2);
+        }
+    }
+    void ani(int[] direction)
+    {
+        playerAnimator.SetFloat("up_down", direction[0]);
+        playerAnimator.SetFloat("left_right", direction[1]);
+        playerAnimator.SetBool("sleep", false);
     }
 }

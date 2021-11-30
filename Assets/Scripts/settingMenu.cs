cf448b1269265cd31617d54eb78e4672b836c5d8
Xiang <bosszheng220@gmail.com>
2021-12-01 02:34:05 +0800

    Alpha2 version 2.0

--- Assets/Scripts/settingMenu.cs
+++ Assets/Scripts/settingMenu.cs
@@ -2,10 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class settingMenu : MonoBehaviour
 {
     [SerializeField] private Slider SESlider, MusicSlider;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 1;
+        for (int i = 0;i < resolutions.Length; ++i)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        options.Add("Full Screen");
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     private void Awake()
     {
         SESlider.value = FindObjectOfType<AudioManager>().getSEAmplifier();
@@ -19,5 +46,19 @@ public class settingMenu : MonoBehaviour
     public void updateMusicAmplifier(float val)
     {
         FindObjectOfType<AudioManager>().updateMusicVolume(val);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        if(resolutionIndex + 1 == resolutions.Length)
+        {
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            Screen.fullScreen = true;
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
     }
 }

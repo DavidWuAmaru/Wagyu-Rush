86bf26e3676121f32b46cd024cdfe71ac0197fb8
hiimjanna <B10815064@gapps.ntust.edu.tw>
2021-12-13 23:52:11 +0800

    WorldMenu Change Page Done

--- Assets/Scripts/cowBehaviorInWorldMenu.cs
+++ Assets/Scripts/cowBehaviorInWorldMenu.cs
@@ -11,37 +11,45 @@ public class cowBehaviorInWorldMenu : MonoBehaviour
     [SerializeField] private float alphaspeed;
     [SerializeField] private Canvas LevelMenu;
     [SerializeField] private Canvas WorldMenu;
+    [SerializeField] private GameObject bgLayer;
     [SerializeField] private TextMeshProUGUI WorldTitle;
+    [SerializeField] private Sprite[] bubbleImg;
     private float moveAmount;
     private float[] alpha;
     private bool controlenable;
     private Vector3 startposition;
-    private int positionIndex,page;
+    private int positionIndex, page;
     private const int itemsNumber = 3;
+    float cowMovePeriod = 0.35f;
+    float bgMovePeriod = 0.5f;
+    private const int maxPages = 2;
+    bool initializaed = false;
     // Start is called before the first frame update
     void Start()
     {
         //²¾°Êªº¶ZÂ÷
-       
+        startposition = transform.position;
+
         controlenable = true;
         alpha = new float[bubbles.Length];
-        for(int i=0;i<bubbles.Length;++i)
+        for (int i = 0; i < bubbles.Length; ++i)
         {
             alpha[i] = bubbles[i].GetComponent<Image>().color.a;
         }
         positionIndex = 0;
         page = 0;
-        
+
+        initializaed = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        startposition = new Vector3(buttons[0].gameObject.transform.position.x,transform.position.y,transform.position.z);
         moveAmount = buttons[1].gameObject.transform.position.x - buttons[0].gameObject.transform.position.x;
+
         for (int i = 0; i < itemsNumber; ++i)
         {
-            bubbles[i + page * itemsNumber].GetComponent<Image>().color = Vector4.Lerp(bubbles[i + page * itemsNumber].GetComponent<Image>().color, new Vector4(1, 1, 1, alpha[i + page * itemsNumber]), alphaspeed);
+            bubbles[i].GetComponent<Image>().color = Vector4.Lerp(bubbles[i].GetComponent<Image>().color, new Vector4(1, 1, 1, alpha[i]), alphaspeed);
         }
 
         if (!controlenable) return;
@@ -51,11 +59,20 @@ public class cowBehaviorInWorldMenu : MonoBehaviour
             if(positionIndex>page*itemsNumber)
             {
                 controlenable = false;
-                alpha[positionIndex] = 0;
+                alpha[positionIndex % itemsNumber] = 0;
                 positionIndex--;
                 StartCoroutine(IECowMove());
             }
-            
+            else
+            {
+                if(page != 0)
+                {
+                    alpha[positionIndex % itemsNumber] = 0;
+                    page--;
+                    positionIndex--;
+                    StartCoroutine(IEbgLayerMove(true));
+                }
+            }
             GetComponent<Image>().sprite = cowleft;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
@@ -63,9 +80,19 @@ public class cowBehaviorInWorldMenu : MonoBehaviour
             if(positionIndex<(page+1)*itemsNumber-1)
             {
                 controlenable = false;
-                alpha[positionIndex] = 0;
+                alpha[positionIndex % itemsNumber] = 0;
                 positionIndex++;
                 StartCoroutine(IECowMove());
+            }
+            else
+            {
+                if(page<maxPages - 1)
+                {
+                    alpha[positionIndex % itemsNumber] = 0;
+                    page++;
+                    positionIndex++;
+                    StartCoroutine(IEbgLayerMove(false));
+                }
             }
 
             GetComponent<Image>().sprite = cowright;
@@ -90,9 +117,24 @@ public class cowBehaviorInWorldMenu : MonoBehaviour
                     MenuButtonFunction.ChapterNumber = 2;
                     buttonMaskManager.currentWorld = 2;
                     break;
+                case 3:
+                    WorldTitle.text = "4.Portal";
+                    MenuButtonFunction.ChapterNumber = 3;
+                    buttonMaskManager.currentWorld = 3;
+                    break;
+                case 4:
+                    WorldTitle.text = "5.Doom";
+                    MenuButtonFunction.ChapterNumber = 4;
+                    buttonMaskManager.currentWorld = 4;
+                    break;
+                case 5:
+                    WorldTitle.text = "6.Custom";
+                    MenuButtonFunction.ChapterNumber = 5;
+                    buttonMaskManager.currentWorld = 5;
+                    break;
                 default:
                     Debug.Log("World Title Text Error");
-                    break;
+                    return;
             }
 
             LevelMenu.gameObject.SetActive(true);
@@ -105,19 +147,63 @@ public class cowBehaviorInWorldMenu : MonoBehaviour
     IEnumerator IECowMove()
     {
         //Debug.Log(moveAmount + " " + positionIndex);
-        Vector3 step = new Vector3(moveAmount, 0, 0) * 3; //speed up
-        Vector3 target = new Vector3((positionIndex % itemsNumber)*moveAmount ,0,0) + startposition;
-        if (target.x < transform.position.x) step *= -1;
-        while (Vector3.Distance(transform.position, target) >= Mathf.Abs(step.x) * Time.deltaTime * 2)
+        Vector3 target = new Vector3((positionIndex % itemsNumber) * moveAmount, 0, 0) + startposition;
+        Vector3 step = new Vector3(moveAmount, 0, 0) * Time.deltaTime / cowMovePeriod; //speed up
+        while (Vector3.Distance(transform.position, target) >= step.x * 1.5f)
         {
-            transform.position += step * Time.deltaTime;
+            step = new Vector3(moveAmount, 0, 0) * Time.deltaTime / cowMovePeriod; //speed up
+            if (target.x < transform.position.x) step *= -1;
+            transform.position += step;
             yield return null;
         }
         transform.position = target;
-        alpha[positionIndex] = 1;
+        alpha[positionIndex % itemsNumber] = 1;
         controlenable = true;
     }
-
+    Vector3 bgLayerDistance = new Vector3(800.0f,0,0);
+    IEnumerator IEbgLayerMove(bool isleft)
+    {
+        controlenable = false;
+        float timeElapsed = 0.0f;
+        Vector3 bgLayerInitialPos = bgLayer.transform.position;
+        Vector3 cowInitialPos = transform.position;
+        Vector3 cowDistance = new Vector3(2 * moveAmount, 0, 0);
+        while(timeElapsed <= bgMovePeriod)
+        {
+            timeElapsed += Time.deltaTime;
+            if(isleft == false)
+            {
+                bgLayer.transform.position = bgLayerInitialPos - bgLayerDistance / bgMovePeriod * timeElapsed;
+                transform.position = cowInitialPos - cowDistance / bgMovePeriod * timeElapsed;
+            }
+            else
+            {
+                bgLayer.transform.position = bgLayerInitialPos + bgLayerDistance / bgMovePeriod * timeElapsed;
+                transform.position = cowInitialPos + cowDistance / bgMovePeriod * timeElapsed;
+            }
+            
+            yield return null;
+        }
+        bgLayer.transform.position = bgLayerInitialPos;
+        if(isleft == false)
+        {
+            transform.position = startposition;
+        }
+        else
+        {
+            transform.position = startposition + new Vector3(2*moveAmount,0,0);
+        }
+        controlenable = true;
+        alpha[positionIndex % itemsNumber] = 1;
+        updateBubbleImg();
+    }
+    void updateBubbleImg()
+    {
+        for (int i = 0; i < itemsNumber; ++i)
+        {
+            buttons[i].transform.GetChild(1).GetComponent<Image>().sprite = bubbleImg[page * itemsNumber + i];
+        }
+    }
     /*
     IEnumerator IEbubbleShow(int index)
     {
@@ -130,5 +216,25 @@ public class cowBehaviorInWorldMenu : MonoBehaviour
     {
         yield return null;
     }*/
+    private void OnEnable()
+    {
 
+    }
+    public void Reset()
+    {
+        if (!initializaed) return;
+
+        positionIndex = 0;
+        page = 0;
+        for (int i = 1; i < itemsNumber; ++i)
+        {
+            alpha[i] = 0;
+            bubbles[i].GetComponent<Image>().color = new Vector4(1, 1, 1, alpha[i]);
+        }
+        alpha[0] = 1;
+        bubbles[0].GetComponent<Image>().color = new Vector4(1, 1, 1, alpha[0]);
+        gameObject.transform.position = startposition;
+        controlenable = true;
+        updateBubbleImg();
+    }
 }
